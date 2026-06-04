2e973a9a96828a0bcf4f33e7a614d8b1ef0fc4d4
Mario Coloma <marioxcolomar@gmail.com>
2026-06-05 00:33:59 +0200

    Add endpoints for api and admin. Add metrics to its own page.

--- main.go
+++ main.go
@@ -16,9 +16,9 @@ func main() {
 	apiCfg := apiConfig{}
 
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
-	mux.HandleFunc("GET /healthz", handleHealthCheck)
-	mux.HandleFunc("GET /metrics", apiCfg.handleMetrics)
-	mux.HandleFunc("POST /reset", apiCfg.handleMetricsReset)
+	mux.HandleFunc("GET /api/healthz", handleHealthCheck)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handleMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handleMetricsReset)
 
 	const port = "8080"
 
@@ -50,9 +50,18 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(200)
-	fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits.Load())
+	page := fmt.Sprintf(`
+<html>
+<body>
+<h1>Welcome, Chirpy Admin</h1>
+<p>Chirpy has been visited %d times!</p>
+</body>
+</html>
+`,
+		cfg.fileserverHits.Load())
+	w.Write([]byte(page))
 }
 
 func (cfg *apiConfig) handleMetricsReset(w http.ResponseWriter, r *http.Request) {
